a8fbb46a7c31a78eebdb183f041cbee63cdbdd3e
IMystyk <57564026+IMystyk@users.noreply.github.com>
2019-12-04 19:40:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+"""That's where all magic happens"""
+import sys
+sys.path.insert(1, 'D:\\Users\\Mystyk\\Desktop\\Mystyk\\Python\\Hangman1.0\\Modules')
+
+import display
+
+
+while True:
+    display.mainmenu()
