e7473fa159a4274587261ac806cf488c88f5c1d2
IMystyk <57564026+IMystyk@users.noreply.github.com>
2019-12-15 18:37:06 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-"""That's where all magic happens"""
-import sys
-sys.path.insert(1, 'D:\\Users\\Mystyk\\Desktop\\Mystyk\\Python\\Hangman1.0\\Modules')
-
-import display
-
-
-while True:
-    display.mainmenu()
